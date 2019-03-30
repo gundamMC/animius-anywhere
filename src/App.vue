2ccb85f7cc94a77cc3c3af44d634d6bfe7087259
gundamMC <gundammc@qq.com>
2019-03-30 01:06:59 -0700

    Set up basic app structure Implement router nativation (with siderbar/drawer) and custom header

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <root>
+      <app-navigation></app-navigation>
+    </root>
+</template>
+
+<script>
+import React, { Component } from 'react';
+import { StackNavigator, DrawerNavigator } from "vue-native-router";
+import { Root } from "native-base";
+
+import HomeScreen from "../src/screens/homeScreen.vue";
+import Sidebar from '../src/screens/sidebar.vue';
+
+const Drawer = DrawerNavigator(
+  {
+    Home: HomeScreen
+  },
+  {
+    initialRouteName: "Home",
+    contentOptions: {
+      activeTintColor: "#e91e63"
+    },
+    contentComponent: props => {
+      // console.log("props inside contentComponent", props);
+      return <Sidebar {...props} />;
+    }
+  }
+);
+const AppNavigation = StackNavigator(
+  {
+    Drawer: Drawer
+  },
+  {
+    initialRouteName: "Drawer",
+    headerMode: "none"  // use custom header defined in each page
+  }
+);
+
+export default {
+    components: { Root, AppNavigation }
+}
+
+</script>
---
+++ src/screens/homeScreen.vue
@@ -0,0 +1,59 @@
+<template>
+  <nb-container>
+    <nb-header>
+      <nb-left>
+        <nb-button transparent>
+          <nb-icon name="menu" />
+        </nb-button>
+      </nb-left>
+      <nb-body>
+        <nb-title>Header</nb-title>
+      </nb-body>
+      <nb-right />
+    </nb-header>
+    <nb-content padder>
+
+      <nb-text>Animius</nb-text>
+
+      <nb-button v-bind:on-press="handleBtnPress">
+        <nb-text>{{message}}</nb-text>
+      </nb-button>
+      
+      <nb-form>
+        <nb-item>
+          <nb-input placeholder="Username" v-model="message" />
+        </nb-item>
+      </nb-form>
+
+    </nb-content>
+
+    <!-- probably will not need a footer  -->
+    <!-- <nb-footer>
+      <nb-footer-tab>
+        <nb-button active full>
+          <nb-text>Footer</nb-text>
+        </nb-button>
+      </nb-footer-tab>
+    </nb-footer> -->
+
+  </nb-container>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      message: 'Hello World!'
+    }
+  },
+  methods: {
+    handleBtnPress: function() {
+      alert('Btn Press');
+    }
+  }
+}
+</script>
+ 
+<style>
+
+</style>
